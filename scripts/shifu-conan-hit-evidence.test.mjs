b2d9c9244775a1bdacf4f52fdaf896d93316117e
Project Cut Merge Queue Admission <project-cut-merge-queue-admission@example.invalid>
2000-01-01 00:00:00 +0000

    fix(shifu): bind exact Conan cache evidence
    Signed-off-by: Keren Dong <keren.dong@kungfu.link>

--- scripts/shifu-conan-hit-evidence.test.mjs
+++ scripts/shifu-conan-hit-evidence.test.mjs
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import assert from 'node:assert/strict';
+import fs from 'node:fs';
+import os from 'node:os';
+import path from 'node:path';
 import test from 'node:test';
 
 import {
+  assertFreshCoreBuildTree,
   graphHitEvidence,
   hitPlan,
   parseHitArgs,
+  validatePublishReceipt,
 } from './shifu-conan-hit-evidence.mjs';
+import { exactClosureDigest } from './shifu-conan-publish.mjs';
 
 test('hit evidence dry-run binds the exact macOS toolchain slice', () => {
   const options = parseHitArgs(['--matrix-entry', 'macos-arm64']);
@@ -20,7 +26,21 @@ test('hit evidence dry-run binds the exact macOS toolchain slice', () => {
   assert.match(output.command, /--build=never/);
 });
 
+test('hit evidence requires an absent Core build tree', (t) => {
+  const root = fs.mkdtempSync(path.join(os.tmpdir(), 'shifu-hit-tree-'));
+  t.after(() => fs.rmSync(root, { recursive: true, force: true }));
+  assert.deepEqual(assertFreshCoreBuildTree(root), { state: 'absent' });
+  fs.mkdirSync(path.join(root, 'framework', 'core', 'build'), {
+    recursive: true,
+  });
+  assert.throws(
+    () => assertFreshCoreBuildTree(root),
+    /requires an absent Core build tree/,
+  );
+});
+
 test('hit evidence accepts only exact downloads from the selected remote', () => {
+  const exact = 'rocksdb/6.29.5#rrev:packageId#prev';
   const evidence = graphHitEvidence(
     {
       graph: {
@@ -41,10 +61,13 @@ test('hit evidence accepts only exact downloads from the selected remote', () =>
       },
     },
     'workhub-conan',
+    [exact],
   );
-  assert.deepEqual(evidence.exactReferences, [
-    'rocksdb/6.29.5#rrev:packageId#prev',
-  ]);
+  assert.deepEqual(evidence.exactReferences, [exact]);
+  assert.equal(
+    evidence.resolvedDependencies[0].effectiveSettings['compiler.cppstd'],
+    undefined,
+  );
   assert.throws(
     () => graphHitEvidence({ graph: { nodes: {} } }, 'workhub-conan'),
     /contains no exact dependency references/,
@@ -68,5 +91,83 @@ test('hit evidence accepts only exact downloads from the selected remote', () =>
         'workhub-conan',
       ),
     /rejected non-remote dependency states/,
+  );
+  assert.throws(
+    () =>
+      graphHitEvidence(
+        {
+          graph: {
+            nodes: {
+              1: {
+                ref: 'rocksdb/6.29.5#rrev',
+                package_id: 'differentPackageId',
+                prev: 'prev',
+                binary: 'Download',
+                binary_remote: 'workhub-conan',
+              },
+            },
+          },
+        },
+        'workhub-conan',
+        [exact],
+      ),
+    /exact closure differs from publisher receipt/,
+  );
+});
+
+test('hit evidence validates and binds a strict publisher receipt', () => {
+  const exactReferences = ['rocksdb/6.29.5#rrev:packageId#prev'];
+  const receipt = {
+    schema: 'shifu.conan-binary-publish-receipt/v1',
+    sourceRevision: 'sourceRevision',
+    matrixEntry: 'macos-arm64',
+    settings: {
+      os: 'Macos',
+      arch: 'armv8',
+      compiler: 'apple-clang',
+      'compiler.version': '21',
+      'compiler.cppstd': '23',
+    },
+    remote: 'workhub-conan',
+    identity: 'rrev-package-id-prev',
+    compatibilityPolicy: 'disabled-for-exact-package-id',
+    conan: {
+      version: 'Conan version 2.test',
+      compatibilityPolicy: 'global-plugin-disabled-in-disposable-home',
+    },
+    lockfile: {
+      schema: 'conan-lockfile',
+      digest: 'sha256:lockfile',
+      pins: 'dependency-rrev',
+    },
+    exactReferences,
+    closureDigest: exactClosureDigest(exactReferences),
+  };
+  assert.deepEqual(
+    validatePublishReceipt(receipt, {
+      matrixEntry: 'macos-arm64',
+      remote: 'workhub-conan',
+      sourceRevision: 'sourceRevision',
+      lockfileDigest: 'sha256:lockfile',
+    }),
+    {
+      sourceRevision: 'sourceRevision',
+      closureDigest: receipt.closureDigest,
+      lockfileDigest: 'sha256:lockfile',
+      exactReferences,
+    },
+  );
+  assert.throws(
+    () =>
+      validatePublishReceipt(
+        { ...receipt, closureDigest: 'sha256:wrong' },
+        {
+          matrixEntry: 'macos-arm64',
+          remote: 'workhub-conan',
+          sourceRevision: 'sourceRevision',
+          lockfileDigest: 'sha256:lockfile',
+        },
+      ),
+    /closure digest is invalid/,
   );
 });
