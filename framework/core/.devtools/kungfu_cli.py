25e842e4161f5f8c34cba36c021faffc51c7d21c
Keren Dong <keren.dong+cx@kungfu.link>
2026-07-26 17:51:07 +0800

    fix(core): preserve dev wrapper boundary
    Signed-off-by: Keren Dong <keren.dong+cx@kungfu.link>

--- framework/core/.devtools/kungfu_cli.py
+++ framework/core/.devtools/kungfu_cli.py
@@ -2,7 +2,6 @@
 
 from env import __frozen__
 from kungfu import __main__ as origin
-from multiprocessing import freeze_support
 from pathlib import Path
 from os import environ
 
@@ -10,11 +9,5 @@ from os import environ
 environ["KUNGFU_DIR"] = Path("dist/kungfu").resolve().as_posix()
 
 
-def main():
-    if __frozen__:
-        freeze_support()
-        origin.main()
-
-
-if __name__ == "__main__":
-    main()
+if __frozen__:
+    origin.main()
