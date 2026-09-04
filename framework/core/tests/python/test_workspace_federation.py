5c32c9c809a2f4d55c305a8d3ba239e9c94d24b4
Keren Dong <keren.dong@kungfu.link>
2026-09-04 09:46:20 +0800

    test(core): preserve workspace autouse fixture
    Signed-off-by: Keren Dong <keren.dong@kungfu.link>

--- framework/core/tests/python/test_workspace_federation.py
+++ framework/core/tests/python/test_workspace_federation.py
@@ -6,3 +6,4 @@ from _workspace_federation_contract_cases import *
 from _workspace_federation_query_cases import *
 from _workspace_federation_retained_cases import *
 from _workspace_federation_verification_cases import *
+from _workspace_federation_support import _bind_work_control_profile
